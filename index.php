638bdcad799b0cc163ae43f3c50f77f3f66b73a0
Shreyas Rathod <shreyasrathod@Shreyass-MacBook-Pro.local>
2018-10-05 19:08:57 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+ 
+// Update the path below to your autoload.php, 
+// see https://getcomposer.org/doc/01-basic-usage.md 
+require_once 'vendor/autoload.php'; 
+ 
+use Twilio\Rest\Client; 
+ 
+$sid    = " "; //add sid
+$token  = " "; //add auth token
+$twilio = new Client($sid, $token); 
+ 
+$message = $twilio->messages 
+                  ->create(" ", // add your own number to send message to 
+                           array( 
+                               "from" => "+12672744008",       
+                               "body" => "Your message" 
+                           ) 
+                  ); 
+ 
+print($message->sid);
